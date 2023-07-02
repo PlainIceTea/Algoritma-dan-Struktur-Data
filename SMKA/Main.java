cef2469a51c5fd75ae7987fda8b1573d8a1ac21e
PlainIceTea <130469962+PlainIceTea@users.noreply.github.com>
2023-07-02 15:37:57 +0700

    Add files via upload
    Perusahaan kereta milik Pak Patur memiliki 4 varian gerbong. Yaitu:
    
    K1: Gerbong eksekutif
    K2: Gerbong bisnis
    K3: Gerbong ekonomi
    KM: Gerbong makan
    Suatu saat karena penumpang yang terus membeludak, Pak Patur ingin membuat inovasi yang memungkinkan kereta bisa memiliki banyak sekali gerbong, bahkan tidak terbatas.
    
    Walau terkesan seperti khayalan, prinsip Pak Patur tetap sama "tidak ada yang tidak mungkin".
    
    Untuk memenuhi keinginan Pak Patur, anda diminta untuk membuat aplikasi perancangan yang bisa menggambarkan rangkaian gerbong kereta tersebut.
    
    Catatan
    
    Implementasi harus dilakukan dengan Singly Linked List
    Setiap perintah ditulis dengan huruf KAPITAL
    Menu perintah dapat diimplementasikan menggunakan infinite loop
    Program hanya akan berhenti ketika ada perintah CETAK

---
+++ SMKA/Main.java
@@ -0,0 +1,102 @@
+package SMKA;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String kereta ;
+        boolean loop = false;
+        rapihdhoho rd = new rapihdhoho();
+        rd.inisialisasi();
+        String namakereta = input.nextLine();
+        while(!loop){
+            kereta = input.nextLine();
+            if (kereta.equalsIgnoreCase("cetak")){
+                System.out.println(namakereta);
+                rd.print();
+                loop = true;
+                break;
+            }
+            String[] arrOfStr = kereta.split(" ");
+            String kata1 = arrOfStr[0];
+            String kata2 = arrOfStr[1];
+            if (kata1.equalsIgnoreCase("tambah")){                          
+                rd.tambahGerbong(kata2);
+            }else if(kata1.equalsIgnoreCase("lepas")){
+                int idx = Integer.parseInt(kata2);
+                rd.lepasGerbong(idx);
+            }
+        
+            
+        }
+        
+    }
+
+
+    static class Node {
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+        }
+    }
+
+    static class rapihdhoho{
+        Node head;
+        Node tail; 
+        int size = 0;
+
+        void inisialisasi(){
+            head = null;
+            tail = null;
+        }
+
+        void tambahGerbong(String data){
+            if (tail == null) {
+                tail = new Node(data);
+                head = tail;
+            } else {
+                tail.next = new Node(data);
+                tail = tail.next;
+            }
+            size++;
+        }
+
+       void lepasGerbong(int idx){
+            if (idx == 0) {
+                if (head == tail) {
+                    head = null;
+                    tail = null;
+                }else head = head.next;
+            }else{
+                Node currNode = head;
+                for (int i=0; i < idx - 1; i++){
+                    currNode = currNode.next;
+                }if (currNode.next == tail) {
+                    currNode.next = null;
+                    tail = currNode;}else{
+                currNode.next = currNode.next.next;}
+            }
+        }
+        
+        
+           
+       void print(){
+            Node currentNode = head;
+            if(currentNode==null){
+                System.out.println("");
+            }else{
+            System.out.print(currentNode.data);
+            currentNode = currentNode.next;
+            while(currentNode != null){
+                System.out.print("-" +currentNode.data );
+                currentNode = currentNode.next;
+            }
+        }
+        }
+        
+    }
+
+}
